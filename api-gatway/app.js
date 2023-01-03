afaf927a9ac5b00dd662c069b12ed8e1ec646e26
Ali Fahrial Anwar <alifahrialanwar@gmail.com>
2023-01-03 13:28:37 +0700

    First Commit

---
+++ api-gatway/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const coursesRouter = require('./routers/courses');
+const mediaRouter = require('./routers/media');
+const ordersRouter = require('./routers/orders');
+const paymentsRouter = require('./routers/payments');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//daftarkan router di express server
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/courses', coursesRouter);
+app.use('/media', mediaRouter);
+app.use('/orders', ordersRouter);
+app.use('/payments', paymentsRouter);
+
+module.exports = app;
